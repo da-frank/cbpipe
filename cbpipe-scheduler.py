3850f378edc206a2ab9deb26224d18b4c44373aa
David Frank <d.frank@uni-luebeck.de>
2023-09-04 15:18:56 +0000

    autoformat

--- cbpipe-scheduler.py
+++ cbpipe-scheduler.py
@@ -5,47 +5,51 @@ from watchdog.observers import Observer, polling
 from watchdog.events import PatternMatchingEventHandler
 
 # class overriding PatternMatchingEventhandler on_created method
+
+
 class CustomHandler(PatternMatchingEventHandler):
     # on_created event handler
     def on_created(self, event):
         # Display the file created event TODO
         logging.info("File created: % s", event.src_path)
- 
+
     # on_modified event handler
     def on_modified(self, event):
         # Display the file modified event TODO
         logging.info("File modified: % s", event.src_path)
- 
+
     # on_deleted event handler
     def on_deleted(self, event):
         # Display the file deleted event TODO
         logging.info("File deleted: % s", event.src_path)
- 
+
     # on_moved event handler
     def on_moved(self, event):
         # Display the file moved event TODO
-        logging.info("File moved: % s to % s", event.src_path, event.dest_path)	
+        logging.info("File moved: % s to % s", event.src_path, event.dest_path)
+
 
 if __name__ == "__main__":
     # Set the format for logging info
     logging.basicConfig(level=logging.INFO,
                         format='%(asctime)s - %(message)s',
                         datefmt='%Y-%m-%d %H:%M:%S')
- 
+
     # Set format for displaying path
     path = "inputs/"
-    #path = "/tmp/inputs"
+    # path = "/tmp/inputs"
 
-# TODO Fix Path. Not possible to get notifications form mounted path on windows host. 
+# TODO Fix Path. Not possible to get notifications form mounted path on windows host.
 
     # initialize regex event handler to ignore ERROR.json files
-    event_handler = CustomHandler(ignore_patterns=["ERROR.json"], ignore_directories=True)
- 
+    event_handler = CustomHandler(
+        ignore_patterns=["ERROR.json"], ignore_directories=True)
+
     # Initialize Observer
-    #observer = Observer()
+    # observer = Observer()
     observer = polling.PollingObserver()
     observer.schedule(event_handler, path, recursive=True)
- 
+
     # Start the observer
     observer.start()
     try:
